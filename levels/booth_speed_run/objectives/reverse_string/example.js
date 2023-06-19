0371d078b8bd5f4a112f36d817ba7aa544fed67e
Marius Obert <obatzer@gmail.com>
2023-06-19 22:25:22 +0200

    On-site improvals and experiments

--- levels/booth_speed_run/objectives/reverse_string/example.js
+++ levels/booth_speed_run/objectives/reverse_string/example.js
@@ -1,2 +1,15 @@
-// TODO:Reverse this string
+// Ignore these lines for now
+const twilio = require("twilio");
+const client = new twilio(process.env.TQ_TWILIO_ACCOUNT_SID, process.env.TQ_TWILIO_AUTH_TOKEN);
+
+// TODO: Reverse this string and send it via SMS
 const string = "!emosewa si oiliwT";
+const reversed = "";
+
+console.log(reversed);
+
+client.messages.create({
+    to: "", // TODO: Insert your number or +49 1573 5996840,
+    from: process.env.TQ_TWILIO_NUMBER,
+    body: reversed
+});
